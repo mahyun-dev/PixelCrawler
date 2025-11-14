350193c995fb56e52b2895f76572f34418aaf2e7
Luminoa <77388181+mahyun-dev@users.noreply.github.com>
2025-11-14 18:06:32 +0900

    Refactor ResourceLoader basePath for resource loading

--- game/src/systems/ResourceLoader.js
+++ game/src/systems/ResourceLoader.js
@@ -2,7 +2,7 @@
 export class ResourceLoader {
     constructor(scene) {
         this.scene = scene;
-        this.basePath = 'PixelCrawler/Pixel-Crawler-Pack';
+        this.basePath = '../Pixel-Crawler-Pack';
     }
 
     // 캐릭터 애니메이션 로드
@@ -145,3 +145,4 @@ export class ResourceLoader {
         this.loadWeapons();
     }
 }
+
